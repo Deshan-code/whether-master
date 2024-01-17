ee704dfa5321564585a36b90294edb06372b68da
Deshan-code <deshanjayawardana52@gmail.com>
2024-01-17 12:35:30 +0530

    commit

---
+++ main.tf
@@ -0,0 +1,16 @@
+# main.tf
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0c7217cdde317cfec"  # Ubuntu AMI ID
+  instance_type = "t2.micro"  # or choose an instance type based on your requirements
+
+  
+  tags = {
+    Name = "example-instance"
+    # Add other tags as needed
+  }
+}
